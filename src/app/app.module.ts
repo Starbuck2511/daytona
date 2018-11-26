047d999a953dad0bd8a5131d4e49b73972661d72
Starbuck2511 <starbuck@gmx-topmail.de>
2018-11-26 13:45:42 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { ContentComponent } from './core/content/content.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { TechnologyStackComponent } from './technology-stack/technology-stack.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ReferencesComponent } from './references/references.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    ContentComponent,
+    HomeComponent,
+    ServicesComponent,
+    TechnologyStackComponent,
+    AboutComponent,
+    ContactComponent,
+    TimelineComponent,
+    ReferencesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/technology-stack/technology-stack.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-technology-stack',
+  templateUrl: './technology-stack.component.html',
+  styleUrls: ['./technology-stack.component.css']
+})
+export class TechnologyStackComponent implements OnInit, OnDestroy {
+
+  timeoutId: any;
+
+  // array with texts to type in typewriter
+  dataText = [
+    'Angular.',
+    'Ionic.',
+    'React.',
+    'Typescript.',
+    'Vanilla JS.',
+    'git.',
+    'svn.',
+    'MySQL.',
+    'MariaDB.',
+    'Oracle.',
+    'MongoDB.',
+    'Jenkins.',
+    'Ansible.',
+    'Vagrant.',
+    'Docker.',
+    'PHP5 PHP7.',
+    'Symfony.',
+    'Composer.',
+    'NodeJS.',
+    'npm.',
+    'gulp.',
+    'Webpack.',
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    // start the text animation
+    this.timeoutId = setTimeout(() => this.startTextAnimation(0), 3000);
+  }
+
+  // start a typewriter animation for a text in the this.dataText array
+  startTextAnimation(i) {
+    if ('undefined' === typeof this.dataText[i]) {
+      this.timeoutId = setTimeout(() => {
+        this.startTextAnimation(0);
+      }, 5000);
+    }
+    // check if this.dataText[i] exists
+    if ('undefined' !== typeof this.dataText[i] && 'undefined' !== typeof this.dataText[i].length) {
+      // text exists start typewriter animation
+      this.typeWriter(this.dataText[i], 0, () => {
+        // after callback (and whole text has been animated), start next text
+        this.startTextAnimation(i + 1);
+      });
+    }
+  }
+
+  // keeps calling itself until the text is finished
+  typeWriter(text, i, fnCallback) {
+    // check if text isn't finished yet
+    if ('undefined' !== typeof text && i < (text.length)) {
+      // add next character to h1
+      document.querySelector('h1.text-animation').innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      this.timeoutId = setTimeout(() => {
+        this.typeWriter(text, i + 1, fnCallback);
+      }, 90);
+    } else if (typeof fnCallback === 'function') {
+      // call callback after timeout
+      this.timeoutId = setTimeout(fnCallback, 1250);
+    }
+  }
+
+  stopTextAnimations() {
+    for (let i = 0; i < this.timeoutId + 100; i++) {
+      clearTimeout(i);
+    }
+  }
+
+  scrollToElement($element): void {
+    $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+
+  ngOnDestroy() {
+    this.stopTextAnimations();
+  }
+
+}

ba7abff1c7bf3b2385ce7e3b2292ec7b1c45d347
Starbuck2511 <starbuck@gmx-topmail.de>
2019-03-03 14:41:55 +0100

    Remove git marker

--- src/app/core/content/content.component.ts
+++ src/app/core/content/content.component.ts
@@ -10,11 +10,7 @@ export class ContentComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-<<<<<<< HEAD
     // test comment 3
-=======
-    // test comment 2
->>>>>>> bf3e5efce4e934331986624f9bdde7dbc6100251
   }
 
 }
